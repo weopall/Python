b50996c4df6140d57e03c884508b98606ff664ab
Андрей Боев <a.boev.it@gmail.com>
2024-05-02 10:38:39 +0300

    Введение

---
+++ 1. Введение/main.py
@@ -0,0 +1,10 @@
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print(num1 + num2)
+
+"""print('Text', 45, True, end=' ')
+print('Text1')
+print('Text 2', 'Text 3', 'Text 4', sep='\n')
+width = 45
+height = 78
+print(f'Прямоугольник со сторонами {width} и {height}')"""
